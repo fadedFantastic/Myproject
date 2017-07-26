10e49b530e0a0411206a47c144cb23de21e16274
fadedFantastic <ryjky123456@gmail.com>
2017-07-26 20:52:49 +0800

    Add files via upload

--- AreaBlock.cs
+++ AreaBlock.cs
@@ -1,36 +1,126 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-[System.Serializable]
 public class AreaBlock : MonoBehaviour
 {
     private static int m_InstanceID = 0;
-    public int InstanceID { get{ return m_InstanceID; } }
 
-    public List<GameObject> blocks = new List<GameObject>();
-    public List<Vector3> handlesPosition = new List<Vector3>();
+    [SerializeField] private List<GameObject> m_Blocks = new List<GameObject>();
+    public List<GameObject> Blocks { get { return m_Blocks; } }
 
+    [SerializeField] private List<Vector3> m_BlockVertices = new List<Vector3>();
+    public List<Vector3> BlockVertices { get { return m_BlockVertices; } }
+
+    private GameObject m_EditObj;
+    private Vector3 m_GizmosSize = Vector3.one;
+    private bool m_Editing = true;
+
+    public void Initialize()
+    {
+        ++m_InstanceID;
+        gameObject.name = "Block" + m_InstanceID.ToString();
+        m_EditObj = new GameObject("edit");
+        m_EditObj.transform.parent = gameObject.transform;
+    }
 
     public void AddBlockObj(Vector3 pos)
     {
-        int count = blocks.Count;
+        int count = m_Blocks.Count;
         if(count > 0)
         {
-            if(Vector3.Distance(blocks[count - 1].transform.position, pos) > float.MinValue)
+            if(Vector3.Distance(m_Blocks[count - 1].transform.position, pos) < 0.001f)
             {
                 return;
             }
         }
 
-        GameObject go = Instantiate(Resources.Load("Cube")) as GameObject;
-
+        InstantiateCubeHandle(pos);
     }
 
+    /// <summary>
+    /// 刷新阻挡区域顶点
+    /// </summary>
+    public void RefreshBlockVertices()
+    {
+        if (!m_Editing) return;
 
+        m_BlockVertices.Clear();
+        foreach(var go in m_Blocks)
+        {
+            if(go)
+            {
+                m_BlockVertices.Add(go.transform.position);
+            }
+        }
+    }
 
+    /// <summary>
+    /// 重建白色立方体，允许编辑阻挡区域
+    /// </summary>
+    public void RebuildCubeHandle()
+    {
+        if (m_EditObj) return;
+
+        m_EditObj = new GameObject("edit");
+        m_EditObj.transform.parent = gameObject.transform;
+
+        int size = m_BlockVertices.Count;
+        for (int i = 0; i < size; ++i)
+        {
+            InstantiateCubeHandle(m_BlockVertices[i]);
+        }
+
+        m_Editing = true;
+    }
+
+    /// <summary>
+    /// 创建白色立方体
+    /// </summary>
+    private void InstantiateCubeHandle(Vector3 pos)
+    {
+        GameObject go = Instantiate(Resources.Load("Cube")) as GameObject;
+        go.name = "Cube";
+        go.transform.position = pos;
+        go.transform.parent = m_EditObj.transform;
+        m_Blocks.Add(go);
+
+        if(m_Editing)
+        {
+            m_BlockVertices.Add(go.transform.position);
+        }
+    }
+
+    public void DestroyCubeHandle()
+    {
+        m_Editing = false;
+        m_Blocks.Clear();
+        DestroyImmediate(m_EditObj);
+    }
+
+#if UNITY_EDITOR
     private void OnDrawGizmos()
     {
+        int size = m_BlockVertices.Count;
+        for (int i = 0; i < size - 1; ++i)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(m_BlockVertices[i], m_BlockVertices[i + 1]);
+        }
+        if (size > 1)
+        {
+            Gizmos.DrawLine(m_BlockVertices[0], m_BlockVertices[size - 1]);
+        }
 
+        // 非编辑模式下辅助显示
+        if(!m_Editing)
+        {
+            for (int i = 0; i < size; ++i)
+            {
+                Gizmos.DrawCube(m_BlockVertices[i], m_GizmosSize);
+            }
+        }
     }
+#endif
 }
--- AreaBlockWindow.cs
+++ AreaBlockWindow.cs
@@ -8,9 +8,15 @@ public class AreaBlockWindow : EditorWindow
 {
     private static AreaBlockWindow m_Instance = null;
     private string m_CurrentSceneName = string.Empty;
-    private GameObject m_Handle = null;
+    private GameObject m_BlockContainer = null;
+    private GameObject m_CurBlock = null;
+    private AreaBlock m_AreaBlock = null;
+    private GameObject m_SphereHandle = null;
     private float m_HandleSize = 1f;
-    private GameObject m_BlockGO = null;
+
+    private Vector2 m_ScrollPos = Vector2.zero;
+    private string m_CurSelectedBlockName = string.Empty;
+    private Color m_OriginGUIColor; 
 
     public static AreaBlockWindow Instance()
     {
@@ -18,11 +24,12 @@ public class AreaBlockWindow : EditorWindow
         {
             m_Instance = GetWindow<AreaBlockWindow>("AreaBlockWindow");
             m_Instance.minSize = new Vector2(400, 300);
+            m_Instance.Initialize();
         }
         return m_Instance;
     }
 
-    private void Awake()
+    private void Initialize()
     {
         m_CurrentSceneName = EditorSceneManager.GetActiveScene().name;
         GenerateBlockContainer();
@@ -36,7 +43,21 @@ public class AreaBlockWindow : EditorWindow
     private void OnDestroy()
     {
         SceneView.onSceneGUIDelegate -= OnSceneGUI;
-        DestroyImmediate(m_Handle);
+        DestroyImmediate(m_SphereHandle);
+
+        AreaBlock[] areaBlockComponents = m_BlockContainer.GetComponentsInChildren<AreaBlock>();
+        foreach(var comp in areaBlockComponents)
+        {
+            comp.DestroyCubeHandle();
+        }
+    }
+
+    private void Update()
+    {
+        if(m_AreaBlock)
+        {
+            m_AreaBlock.RefreshBlockVertices();
+        }
     }
 
     private void OnGUI()
@@ -46,62 +67,119 @@ public class AreaBlockWindow : EditorWindow
         EditorGUILayout.EndVertical();
 
         EditorGUILayout.BeginVertical("Box", GUILayout.Width(200));
-        if(GUILayout.Button("加载阻挡"))
-        {
-
-        }
+        GUILayout.Label("功能区域面板：", "textArea");
         if (GUILayout.Button("创建阻挡"))
         {
-            GenerateHandle();
+            CreateAreaBlock();
         }
-        if (GUILayout.Button("修改阻挡"))
+        if (GUILayout.Button("编辑阻挡"))
         {
-
+            EditAreaBlock();
         }
         if (GUILayout.Button("删除阻挡"))
         {
-
+            DestroyImmediate(m_CurBlock);
         }
+        EditorGUILayout.EndVertical();
+
+        EditorGUILayout.BeginVertical("box", GUILayout.Width(200));
+        GUILayout.Label("阻挡区域列表：", "textArea");
+        m_ScrollPos = GUILayout.BeginScrollView(m_ScrollPos, false, true);
+        foreach (Transform transform in m_BlockContainer.transform)
+        {
+            string blockName = transform.name;
+            if(m_CurSelectedBlockName != string.Empty && m_CurSelectedBlockName == blockName)
+            {
+                m_OriginGUIColor = GUI.backgroundColor;
+                GUI.backgroundColor = Color.green;
+                if(GUILayout.Button(blockName))
+                {
+                    OnSelectBlockButton(transform);
+                }
+                GUI.backgroundColor = m_OriginGUIColor;
+            }
+            else
+            {
+                if(GUILayout.Button(blockName))
+                {
+                    OnSelectBlockButton(transform);
+                    m_CurSelectedBlockName = blockName;
+                }
+            }
+        }
+        GUILayout.EndScrollView();
         EditorGUILayout.EndVertical();
     }
 
-    void GenerateHandle()
+    void CreateAreaBlock()
     {
-        if (!m_Handle)
-        {
-            m_Handle = new GameObject("[Handle]");
-        }
-        Selection.activeGameObject = m_Handle;
+        SelectSphereHandle();
+        m_CurBlock = new GameObject();
+        m_CurBlock.transform.parent = m_BlockContainer.transform;
+        m_AreaBlock = m_CurBlock.AddComponent<AreaBlock>();
+        m_AreaBlock.Initialize();
+        m_CurSelectedBlockName = m_CurBlock.name;
     }
 
+    void EditAreaBlock()
+    {
+        if(m_AreaBlock)
+        {
+            m_AreaBlock.RebuildCubeHandle();
+            SelectSphereHandle();
+        }
+    }
+
+    void OnSelectBlockButton(Transform _transform)
+    {
+        m_CurBlock = _transform.gameObject;
+        m_AreaBlock = _transform.GetComponent<AreaBlock>();
+        Selection.activeGameObject = _transform.gameObject;
+    }
+
+    void SelectSphereHandle()
+    {
+        if (!m_SphereHandle)
+        {
+            m_SphereHandle = new GameObject("[Handle]");
+        }
+        Selection.activeGameObject = m_SphereHandle;
+    }
+
+    /// <summary>
+    /// 生成阻挡区域容器
+    /// </summary>
     void GenerateBlockContainer()
     {
-        m_BlockGO = GameObject.Find("[BlockContainer]");
-        if (!m_BlockGO)
+        m_BlockContainer = GameObject.Find("[BlockContainer]");
+        if (!m_BlockContainer)
         {
-            m_BlockGO = new GameObject("[BlockContainer]");
+            m_BlockContainer = new GameObject("[BlockContainer]");
         }
     }
 
-    void CreateBlock()
+    void AddSubBlock()
     {
-        
+        if(m_CurBlock)
+        {
+            m_AreaBlock.AddBlockObj(m_SphereHandle.transform.position);
+        }
     }
 
     void OnSceneGUI(SceneView view)
     {
         Event current = Event.current;
-        switch (current.type)
+        if(current.type == EventType.KeyDown)
         {
-            case EventType.keyDown:
-                if (current.isKey && current.keyCode == KeyCode.C)
-                    CreateBlock();
-                break;
+            if (current.isKey && current.keyCode == KeyCode.C)
+            {
+                AddSubBlock();
+            }
         }
 
-        if(m_Handle)
+        if(m_SphereHandle)
         {
-            Transform transform = m_Handle.transform;
+            Transform transform = m_SphereHandle.transform;
             Handles.color = Color.red;
             Handles.SphereHandleCap(0, transform.position, transform.rotation, m_HandleSize, EventType.Repaint);
         }
